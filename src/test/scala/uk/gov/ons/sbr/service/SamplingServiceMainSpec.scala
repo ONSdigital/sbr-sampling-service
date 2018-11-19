d42f6cceb7bc6709c6d3f3adce72b6ac2eaa2ae5
vladshiligin <vladtoldmetodoit@gmail.com>
2018-11-19 10:56:29 +0000

    refactored for better testability

--- src/test/scala/uk/gov/ons/sbr/service/SamplingServiceMainSpec.scala
+++ src/test/scala/uk/gov/ons/sbr/service/SamplingServiceMainSpec.scala
@@ -1,18 +1,37 @@
 package uk.gov.ons.sbr.service
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FreeSpec, Matchers}
 import uk.gov.ons.sbr.SamplingTestData
 import uk.gov.ons.sbr.helpers.TestSessionManager
 import org.apache.hadoop.fs.Path
+import org.apache.spark.sql.types.StructType
+import uk.gov.ons.sbr.service.repository.hive.HiveUnitFrameRepository
 import uk.gov.ons.sbr.service.validation.SampleMethodArguments
 
+import scala.util.{Success, Try}
+
 class SamplingServiceMainSpec extends FreeSpec with Matchers with MockFactory with SamplingTestData{
+
+  trait MockServiceValidation{
+  }
+
+  trait MockHiveUnitFrameRepository extends HiveUnitFrameRepository{
+    override def retrieveTableAsDataFrame(unitFrameDatabaseAndTableName: String)
+                                         (implicit spark: SparkSession): Try[DataFrame] = unitFrameDatabaseAndTableName match{
+
+
+      case ";" => Success(spark.createDataFrame(spark.sparkContext.emptyRDD[Row],new StructType()))
+      case "" => Success(spark.createDataFrame(spark.sparkContext.emptyRDD[Row],new StructType()))
+    }
+  }
 
   private trait Fixture {
     val aSparkSession = TestSessionManager.sparkSession
   }
+
+
 
   "A sample" - {
     "is created and exported" - {
